30259eebdb8de60442a7093818c7adc4b186d939
Imran176 <79358307+Imran176@users.noreply.github.com>
2022-12-01 17:49:29 +0500

    implemented redux-persist

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import {createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
@@ -7,4 +8,6 @@ const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store)
+
+export { store, persistor };
